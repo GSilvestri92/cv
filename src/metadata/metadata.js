5f2d4e66b14e99a2a5db76ed2a6b06e3acc09615
G.Silvestri <giulio.silve92@gmail.com>
2024-04-14 19:16:06 +0200

    Yet Another Commit

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -11,19 +11,19 @@ module.exports = {
     ['BigQuery', 100],
     ['Google Cloud Storage', 80],
     ['Problem Solving', 100],
-    ['Erotic', 60],
-    ['Sonnets', 100],
-    ['Plays', 100],
-    ['Writing', 100],
-    ['Directing', 80],
-    ['Literature', 100],
-    ['Influence', 100],
+    ['', 60],
+    ['', 100],
+    ['', 100],
+    ['', 100],
+    ['', 80],
+    ['', 100],
+    ['', 100],
   ],
   positions: [
     {
       title: 'Advanced Data Engineer',
       period: '03/2024 – Current',
-      skills: ['tragedy', 'philosophy', 'death'],
+      skills: ['GCP', 'Python', 'SQL'],
       contents: `
 Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
 `
@@ -31,7 +31,7 @@ Prince Hamlet is visited by his father's ghost and ordered to avenge his father'
     {
       title: 'Data Engineer',
       period: '09/2022 – 03/2024',
-      skills: ['comedy', 'love'],
+      skills: ['GCP', 'Python', 'SQL'],
       contents: `
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
 `
@@ -40,17 +40,15 @@ Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebast
   certification: [
     {
       header: 'Google Cloud Professional Data Engineer',
-      skills: ['theater', 'play'],
+      skills: ['GCP', 'Python', 'SQL'],
       contents: `
-      <a href="www.google.com"></a>
 `
     },
     {
       header: 'Google Cloud Associate Cloud Engineer',
-      skills: ['poetry', 'erotic'],
+      skills: ['GCP', 'Python', 'SQL'],
       contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      `
     }
   ]
 };
