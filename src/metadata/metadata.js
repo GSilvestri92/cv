20fbed1065da0b5e484ec58895460b3c322bb8aa
G.Silvestri <giulio.silve92@gmail.com>
2024-04-14 19:02:25 +0200

    Second Commit

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -6,11 +6,11 @@ module.exports = {
     'Email': '<a href="mailto:giulio.silve92@gmail.com"><i class="fa fa-envelope fact-icon"></i>giulio.silve92@gmail.com</a>'
   },
   skills: [
-    ['Comedy', 100],
-    ['Love', 100],
-    ['Theater', 100],
-    ['Tragedy', 100],
-    ['Poetry', 100],
+    ['Python', 100],
+    ['SQL', 100],
+    ['BigQuery', 100],
+    ['Google Cloud Storage', 80],
+    ['Problem Solving', 100],
     ['Erotic', 60],
     ['Sonnets', 100],
     ['Plays', 100],
@@ -21,40 +21,32 @@ module.exports = {
   ],
   positions: [
     {
-      title: 'Hamlet',
-      period: 'Likely early 17th century',
+      title: 'Advanced Data Engineer',
+      period: '03/2024 – Current',
       skills: ['tragedy', 'philosophy', 'death'],
       contents: `
 Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
 `
     },
     {
-      title: 'Twelfth Night',
-      period: '1600 – 1601',
+      title: 'Data Engineer',
+      period: '09/2022 – 03/2024',
       skills: ['comedy', 'love'],
       contents: `
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
 `
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
     }
   ],
-  experience: [
+  certification: [
     {
-      header: 'Performance',
+      header: 'Google Cloud Professional Data Engineer',
       skills: ['theater', 'play'],
       contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
+      <a href="www.google.com"></a>
 `
     },
     {
-      header: 'Poetry',
+      header: 'Google Cloud Associate Cloud Engineer',
       skills: ['poetry', 'erotic'],
       contents: `
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
